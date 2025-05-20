0ae2c08005f2f675908b2b057defe908ea59147a
Novoemerson <novoemerson@gmail.com>
2025-05-19 21:09:45 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// config.js - Configurações gerais do Tricolor-SP
+
+const CONFIG = {
+    appName: "Tricolor-SP",
+    version: "1.0.0",
+    environment: "development", // Pode ser 'production' em ambiente final
+    apiBaseUrl: "https://api.tricolorsp.com", // Ajuste conforme necessário
+};
+
+module.exports = CONFIG;
