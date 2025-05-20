ebfffc0b120d8b8397ea866365c7fbd6d02e05c6
Novoemerson <novoemerson@gmail.com>
2025-05-19 21:05:47 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// app.js - Arquivo principal do Tricolor-SP
+console.log("Bem-vindo ao Tricolor-SP! 🚀🔥");
+
+// Função de inicialização
+function iniciarProjeto() {
+    console.log("O sistema está rodando corretamente!");
+}
+
+// Chamada da função
+iniciarProjeto();
